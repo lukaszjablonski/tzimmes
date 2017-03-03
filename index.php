3d389ac09723e75658b5b249aa107a33d4aa2581
Lukasz Jablonski <lukasz.jablonski@wp.eu>
2017-03-03 14:58:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
-session_start();
-if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+ session_start();
+?>
+<html lang = "en">
+ <head>
+  <title>Index</title>
+ </head>
+ <body>
+  <?
+   if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) { 
     echo "Welcome to the member's area, " . $_SESSION['username'] . "!";
-} else {
+   }
+   else {
     echo "Please log in first to see this page.";
-}
+   }
+  ?>
+ </body>
+</html>
