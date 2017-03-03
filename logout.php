ffc8316e1e200e7664034d58e1ecdb2d7b9dd2a2
Lukasz Jablonski <lukasz.jablonski@wp.eu>
2017-03-03 15:04:23 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,8 +1,8 @@
 <?php
    session_start();
    unset($_SESSION["username"]);
-   unset($_SESSION["password"]);
+   unset($_SESSION["loggedin"]);
    
-   echo 'You have logged out';
+   echo 'You have cleaned session';
    header('Refresh: 2; URL = login.php');
 ?>
